a5ce0637304971cfaaf06919c14f60e23c7491eb
xSkinnyPete <emeliapw@mail.ru>
2018-08-19 20:48:48 +0300

    1.3. JavaDoc. Code convention.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate.
+ * @author Nikita Emelyanov
+ * @since 19.08.2018
+ */
 public class Calculate {
 	
+/**
+ * Main.
+ * @param args - args.
+ */
 	public static void main (String[] args) {
 		System.out.println("Hello World");
 	}
