ae0e4fddf16814811f81e728377ebed004fc63f7
xSkinnyPete <emeliapw@mail.ru>
2018-08-24 23:31:21 +0300

    Text

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -2,16 +2,26 @@ package ru.job4j.calculate;
 
 /**
  * Calculate.
- * @author Nikita Emelyanov
- * @since 19.08.2018
+ *
+ * @author Nikita Emelyanov (emeliapw@mail.ru)
+ * @version $Id$
+ * @since 0.1
  */
 public class Calculate {
-	
-/**
- * Main.
- * @param args - args.
- */
-	public static void main (String[] args) {
-		System.out.println("Hello World");
-	}
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
