6cb24b5f58e6e564e7f618426927b37245cfbfa8
xSkinnyPete <emeliapw@mail.ru>
2018-08-26 12:41:48 +0300

    3.1. Элементарный калькулятор

---
+++ chapter_001/src/main/java/ru/job4j/calculator/CalculatorTest.java
@@ -0,0 +1,63 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Nikita Emelyanov (emeliapw@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class CalculatorTest {
+    /**
+     * Test Add
+     */
+    @Test
+    public void whenAddOnePlusOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.add(1D, 1D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test Sub
+     */
+    @Test
+    public void whenSubtractFiveSubTwoThenThree() {
+	Calculator calc = new Calculator();
+	calc.div(5D, 2D);
+	double result = calc.getResult();
+	double expected = 3D;
+	assertThat(result, is(expected));
+    }
+
+    /**
+     * Test Div
+     */
+    @Test
+    public void whenDivisionSixDivTwoThenThree() {
+        Calculator calc = new Calculator();
+        calc.div(6D, 2D);
+        double result = calc.getResult();
+        double expected = 3D;
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test Multiple
+     */
+    @Test
+    public void whenMultipleFourMultTwoThenEight() {
+	Calculator calc = new Calculator();
+	calc.mult(4D, 2D);
+	double result = calc.getResult();
+	double expected = 8D;
+	assertThat(result, is(expected));
+    }
+}
