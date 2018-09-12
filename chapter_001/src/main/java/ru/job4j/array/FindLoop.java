8098ba4be11fb59109d3bb2a879d364cf1d86868
xSkinnyPete <emeliapw@mail.ru>
2018-09-12 20:50:49 +0300

    6.1 Добавил тесты

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,10 +1,10 @@
 package ru.job4j.array;
 
 public class FindLoop {
-    public int indexOf(int[] data, int e1) {
+    public int indexOf(int[] data, int el) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
-        for (int index = 0; index < data.length; index++) {
-            if (data[index] == e1) {
+        for (int index = 0; index < data.length;index++) {
+            if (data[index] == el) {
                 rst = index;
                 break;
             }
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -7,19 +7,19 @@ import static org.junit.Assert.assertThat;
 public class FindLoopTest {
 
     @Test
-    public void whenArrayHasLengh5Then0() {
+    public void whenArrayHasLengh3Then2() {
         FindLoop find = new FindLoop();
-        int[] input = new int[]{5, 10, 3};
-        int value = 5;
+        int[] input = new int[]{5, 23, 3};
+        int value = 3;
         int result = find.indexOf(input, value);
-        int expect = 0;
+        int expect = 2;
         assertThat(result, is(expect));
     }
     @Test
-    public void whenArrayHasLengh10Then0() {
+    public void whenArrayHasLengh8ThenError() {
         FindLoop find = new FindLoop();
-        int[] input = new int[]{10, 18, 2};
-        int value = 20;
+        int[] input = new int[]{7, 19, 2};
+        int value =8;
         int result = find.indexOf(input, value);
         int expect = -1;
         assertThat(result, is(expect));
@@ -31,6 +31,15 @@ public class FindLoopTest {
         int value = 25;
         int result = find.indexOf(input, value);
         int expect = 0;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayHasLengh19Then1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[]{6, 19, 17};
+        int value = 19;
+        int result = find.indexOf(input, value);
+        int expect = 1;
         assertThat(result, is(expect));
     }
 }
